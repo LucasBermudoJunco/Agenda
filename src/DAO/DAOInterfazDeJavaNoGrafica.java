eec4d100d2502ddebb347e5172eb7ae5dbb6a8dd
lucas <lucasbjunco@gmail.com>
2024-08-19 17:44:19 +0200

    V2.1 - Avance 3 de la versión. Contenido de este commit: · Creación de las ramas ´´V2.1-InterfazGrafica``, ´´V2.1-Controlador``, ´´V2.1-DAO`` y ´´V2.1-Integración`` para poder especializar cada rama en cada sección de la aplicación (estableciendo la rama ´´V2.1-Integración`` para traer todos los cambios de las demás ramas y así poder ejercutar el programa en cada momento que se requiera para comprobar el progreso de la aplicación · Creación de la interfaz de java ´´DAOInterfazDeJavaNoGrafica`` en el paquete DAO para posteriormente implementar dicha interfaz de java en todas las clases DAO (excepto en ´´DAOGeneral``) · Creación del paquete ´´Controlador`` con la clase ´´Controlador`` (por ahora vacía) para gestionar todas las operaciones del programa

---
+++ src/DAO/DAOInterfazDeJavaNoGrafica.java
@@ -0,0 +1,10 @@
+package DAO;
+
+public interface DAOInterfazDeJavaNoGrafica {
+    
+    boolean create(String fichero);
+    boolean read(String fichero);
+    boolean update(String fichero);
+    boolean delete(String fichero);
+    
+}
