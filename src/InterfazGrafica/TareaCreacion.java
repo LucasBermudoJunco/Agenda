10b57190cc2481216e6b51b6e6e56a65a64ac3fe
lucas <lucasbjunco@gmail.com>
2024-09-02 21:37:55 +0200

    V2.1 - Avance 8 de la versión. Contenido de este commit: · Establecimiento muy avanzado de la parte gráfica de la ventana ´´TareaCreacion``

--- src/InterfazGrafica/TareaCreacion.java
+++ src/InterfazGrafica/TareaCreacion.java
@@ -1,6 +1,13 @@
 package InterfazGrafica;
 
+import Controlador.Controlador;
+
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.InputMismatchException;
 import javax.swing.border.Border;
 import javax.swing.*;
 
@@ -8,13 +15,16 @@ public class TareaCreacion extends JInternalFrame {
     
     /// Atributo(s)
     private static final long serialVersionUID = 1L;
-    private JDesktopPane desktopPane;
-    private JPanel panelTareaCreacion;
-    private JLabel lblTareaCreacionCabecera;
-    private JLabel lblTareaCreacionHoraDeLaTarea;
-    private JLabel lblTareaCreacionContenidoDeLaTarea;
-    private JTextField textFieldTareaCreacionHoraDeLaTarea;
-    private JTextArea textAreaTareaCreacionContenidoDeLaTarea;
+    private final JDesktopPane desktopPane;
+    //private final JPanel panelTareaCreacion;
+    private final JLabel lblTareaCreacionCabecera;
+    private final JLabel lblTareaCreacionHoraDeLaTarea;
+    private final JLabel lblTareaCreacionContenidoDeLaTarea;
+    private final JTextField textFieldTareaCreacionHoraDeLaTarea;
+    private final JTextArea textAreaTareaCreacionContenidoDeLaTarea;
+    private final JButton btnTareaCreacionCrearTarea;
+    private final JScrollPane scrollPaneTareaCreacion;
+    private Controlador controlador;
     
     /// Constructor
     public TareaCreacion(JDesktopPane desktopPane) {
@@ -34,31 +44,92 @@ public class TareaCreacion extends JInternalFrame {
         getContentPane().add(lblTareaCreacionCabecera);
         
         lblTareaCreacionHoraDeLaTarea = new JLabel("Hora:");
-        lblTareaCreacionHoraDeLaTarea.setBounds(69, 72, 46, 14);
+        lblTareaCreacionHoraDeLaTarea.setBounds(29, 72, 46, 14);
         getContentPane().add(lblTareaCreacionHoraDeLaTarea);
         
         lblTareaCreacionContenidoDeLaTarea = new JLabel("Contenido:");
-        lblTareaCreacionContenidoDeLaTarea.setBounds(69, 97, 70, 14);
+        lblTareaCreacionContenidoDeLaTarea.setBounds(29, 101, 72, 14);
         getContentPane().add(lblTareaCreacionContenidoDeLaTarea);
         
         textFieldTareaCreacionHoraDeLaTarea = new JTextField();
-        textFieldTareaCreacionHoraDeLaTarea.setBounds(148, 69, 86, 20);
+        textFieldTareaCreacionHoraDeLaTarea.setBounds(111, 69, 86, 20);
         getContentPane().add(textFieldTareaCreacionHoraDeLaTarea);
         textFieldTareaCreacionHoraDeLaTarea.setColumns(10);
         
         textAreaTareaCreacionContenidoDeLaTarea = new JTextArea();
-        textAreaTareaCreacionContenidoDeLaTarea.setBounds(149, 97, 164, 100);
+        textAreaTareaCreacionContenidoDeLaTarea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_TAB) {
+                    btnTareaCreacionCrearTarea.requestFocus();
+                    e.consume();
+                }
+            }
+        });
+        textAreaTareaCreacionContenidoDeLaTarea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_TAB && e.isShiftDown()) {
+                    textFieldTareaCreacionHoraDeLaTarea.requestFocus();
+                }
+            }
+        });
+        textAreaTareaCreacionContenidoDeLaTarea.setBounds(111, 97, 296, 100);
         Border bordeDelTextAreaTareaCreacionContenidoDeLaTarea = BorderFactory.createLineBorder(Color.BLACK,1);
         textAreaTareaCreacionContenidoDeLaTarea.setBorder(bordeDelTextAreaTareaCreacionContenidoDeLaTarea);
         getContentPane().add(textAreaTareaCreacionContenidoDeLaTarea);
         
-        JButton btnTareaCreacionCrearTarea = new JButton("Crear Tarea");
-        btnTareaCreacionCrearTarea.setBounds(148, 208, 106, 23);
+        btnTareaCreacionCrearTarea = new JButton("Crear Tarea");
+        btnTareaCreacionCrearTarea.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                crearTarea();
+            }
+        });
+        btnTareaCreacionCrearTarea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    crearTarea();
+                } else if(e.getKeyCode() == KeyEvent.VK_TAB && e.isShiftDown()) {
+                    textFieldTareaCreacionHoraDeLaTarea.requestFocus();
+                }
+            }
+        });
+        btnTareaCreacionCrearTarea.setBounds(111, 208, 106, 23);
         getContentPane().add(btnTareaCreacionCrearTarea);
         
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(84, 253, 277, 116);
-        getContentPane().add(scrollPane);
+        scrollPaneTareaCreacion = new JScrollPane();
+        scrollPaneTareaCreacion.setBounds(111, 254, 296, 116);
+        getContentPane().add(scrollPaneTareaCreacion);
+    }
+    
+    /// Método(s)
+    public void crearTarea() {
+        
+        controlador = new Controlador();
+        
+        String horaDeLaTarea = textFieldTareaCreacionHoraDeLaTarea.getText();
+        String contenidoDeLaTarea = textAreaTareaCreacionContenidoDeLaTarea.getText();
+        
+        if(horaDeLaTarea.isBlank() || contenidoDeLaTarea.isBlank()){
+            JOptionPane.showMessageDialog(this, "La hora y el contenido de la tarea " +
+                    "tienen que estar rellenados");
+        } else{
+            boolean horaIntroducidaCorrectamente = true;
+            
+            try{
+                if(Integer.valueOf(horaDeLaTarea) < 0){
+                    horaIntroducidaCorrectamente = false;
+                }
+            } catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(this, "La hora introducida tiene que ser un número entero positivo (sin decimales)");
+            }
+            
+            if(!horaIntroducidaCorrectamente) {
+                JOptionPane.showMessageDialog(this, "La hora introducida tiene que ser un número entero positivo (sin decimales)");
+            }
+        }
+        
     }
 }
-
