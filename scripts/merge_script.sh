e2c10ae5abd153c422b3644637fe9676e221ec78
LucasBermudoJunco <lucasbjunco@gmail.com>
2024-09-13 03:08:55 +0200

    V2 Contenido de este commit: · Modificación del script ´´merge_script.sh``

--- scripts/merge_script.sh
+++ scripts/merge_script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Pedir el nombre de la rama al usuario
-read -p "Introduce el nombre de la rama que quieres fusionar en ´´integracion``: " source_branch
+read -p "Introduce el nombre de la rama que quieres fusionar en integracion: " source_branch
 
 # Lista de ramas a las que se quiere hacer merge desde ´´integracion``
 branches=("V2.1-InterfazGrafica" "V2.1-Controlador" "V2.1-BaseDeDatos")
 
 # Cambiar a la rama ´´integracion`` y hacer merge de la rama ingresada
-echo "Cambiando a ´´integracion`` y haciendo merge de ´´$source_branch``..."
+echo "Cambiando a integracion y haciendo merge de $source_branch..."
 git checkout V2.1-Integracion
 git merge $source_branch
 
 # Hacer push de los cambios en ´´integracion``
-echo "Haciendo push de ´´integracion``..."
+echo "Haciendo push de integracion..."
 git push origin V2.1-Integracion
 
 # Iterar sobre las demás ramas y hacer merge de ´´integracion`` en ellas, excepto la rama de origen
@@ -20,17 +20,25 @@ for branch in "${branches[@]}"
 do
     # Evitar hacer merge en la rama de origen
     if [ "$branch" != "$source_branch" ]; then
-        echo "Cambiando a $branch y haciendo merge de develop..."
+        echo "Cambiando a $branch y haciendo merge de integracion..."
 
         # Cambiar a la rama actual
         git checkout $branch
 
         # Hacer merge de develop en la rama actual
-        git merge develop
+        git merge integracion
+
+        echo
+        echo "Merge de integracion completado"
+        echo
 
         # Hacer push de los cambios en la rama actual
         echo "Haciendo push de $branch..."
         git push origin $branch
+
+        echo
+        echo "push de $branch completado"
+        echo
     else
         echo "Saltando $branch porque es la rama de origen."
     fi
