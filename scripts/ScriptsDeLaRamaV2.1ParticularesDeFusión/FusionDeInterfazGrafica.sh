8599948702289f0e285e057f9237f0dbb5b5f478
LucasBermudoJunco <lucasbjunco@gmail.com>
2024-09-15 14:18:04 +0200

    V2.1-InterfazGrafica Contenido de este commit: · Incorporación, en la carpeta ´´scripts`` en su subcarpeta ´´ScriptsDeLaRamaV2.1ParticularesDeFusion`` en su script ´´FusionDeInterfazGrafica.sh``, de los mensajes ´´echo`` para indicar al usuario, en la terminal donde esté ejecutando el script, todos los pasos que estarán siendo seguidos (que son checkout, merge y push de todas las ramas teniendo como origen de dicho proceso la rama en la que se han  producido los cambios que estarán siendo comfirmados y empujados en git)

--- scripts/ScriptsDeLaRamaV2.1ParticularesDeFusión/FusionDeInterfazGrafica.sh
+++ scripts/ScriptsDeLaRamaV2.1ParticularesDeFusión/FusionDeInterfazGrafica.sh
@@ -1,20 +1,56 @@
 # Rama Integracion
 git checkout V2.1-Integracion
 
+echo
+echo "Cambio realizado a la rama Integracion"
+echo
+
 git git merge V2.1-InterfazGrafica
 
+echo
+echo "Fusión realizada de la rama Integracion con la rama InterfazGrafica"
+echo
+
 git push origin V2.1-Integracion
+
+echo
+echo "Push realizado de la rama Integracion"
+echo
 
 # Rama Controlador
 git checkout V2.1-Controlador
 
+echo
+echo "Cambio realizado a la rama Controlador"
+echo
+
 git merge V2.1-Integracion
 
+echo
+echo "Fusión realizada de la rama Controlador con la rama Integracion"
+echo
+
 git push origin V2.1-Controlador
+
+echo
+echo "Push realizado de la rama Controlador"
+echo
 
 # Rama BaseDeDatos
 git checkout V2.1-BaseDeDatos
 
+echo
+echo "Cambio realizado a la rama BaseDeDatos"
+echo
+
 git merge V2.1-Integracion
 
+echo
+echo "Fusión realizada de la rama BaseDeDatos con la rama Integracion"
+echo
+
 git push origin V2.1-Controlador
+
+echo
+echo "Push realizado de la rama BaseDeDatos"
+echo
