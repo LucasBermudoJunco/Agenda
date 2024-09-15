f87494d2bb4717567fc5af3c5067b0d2ae4c87d6
LucasBermudoJunco <lucasbjunco@gmail.com>
2024-09-15 13:11:57 +0200

    V2.1-BaseDeDatos Contenido de este commit: · Creación, en la carpeta ´´scripts`` en su subcarpeta ´´ScriptsDeLaRamaV2.1ParticularesDeFusión``, del script ´´FusionDeInterfazGrafica.sh`` para realizar automáticamente la fusión de todas las demás ramas con los cambios en la rama V2.1-InterfazGrafica

---
+++ scripts/ScriptsDeLaRamaV2.1ParticularesDeFusión/FusionDeInterfazGrafica.sh
@@ -0,0 +1,20 @@
+# Rama Integracion
+git checkout V2.1-Integracion
+
+git git merge V2.1-InterfazGrafica
+
+git push origin V2.1-Integracion
+
+# Rama Controlador
+git checkout V2.1-Controlador
+
+git merge V2.1-Integracion
+
+git push origin V2.1-Controlador
+
+# Rama BaseDeDatos
+git checkout V2.1-BaseDeDatos
+
+git merge V2.1-Integracion
+
+git push origin V2.1-Controlador
